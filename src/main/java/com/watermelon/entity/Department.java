f67dc88002b62165ce24e5fc0362ec87d1ebf770
Watermelon <1050364782@qq.com>
2020-04-29 00:27:06 +0800

    evaluation v0.0.6 des:Department function import

---
+++ src/main/java/com/watermelon/entity/Department.java
@@ -0,0 +1,15 @@
+package com.watermelon.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    private int id;
+    private String name;
+
+}
---
+++ src/main/java/com/watermelon/service/DepartmentServiceImpl.java
@@ -0,0 +1,35 @@
+package com.watermelon.service;
+
+import com.watermelon.entity.Department;
+import com.watermelon.mapper.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    private DepartmentMapper departmentMapper;
+
+    @Override
+    public void addDepartment(Department department) {
+        departmentMapper.addDepartment(department);
+    }
+
+    @Override
+    public List<Department> listDepartment() {
+        return departmentMapper.listDepartment();
+    }
+
+    @Override
+    public void updateDepartment(Department department) {
+        departmentMapper.updateDepartment(department);
+    }
+
+    @Override
+    public void deleteDepartment(int id) {
+        departmentMapper.deleteDepartment(id);
+    }
+}
